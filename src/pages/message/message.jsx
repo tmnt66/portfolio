c28f0bed67be1fdb4f75ae8241b34579bace989b
Branzil <branzil.nbj@gmail.com>
2022-06-23 22:09:02 +0530

    23 june 2

--- src/pages/message/message.jsx
+++ src/pages/message/message.jsx
@@ -1,17 +1,18 @@
 import './message.css'
 import Button1 from '../../components/button1/button.jsx'
-
+import '../../general.css'
 const Message  = (props)=>{ 
     return(
-     <div className="message">
+     <div className="message font">
+        <h1>Fill Your Contact Details</h1>
               <div className="name1">
-                <input placeholder="Enter your firstname" type="text" />
-                <input placeholder="Enter your lastname" type="text" />
+                <input placeholder=" Company Name" type="text" />
+                <input placeholder=" Job Profile" type="text" />
             </div>
-            <input placeholder="Enter your Number" type="text" />
-            <input placeholder="Enter your E-mail" type="text" />
-            <input placeholder="Enter your Company Name" type="text" />
-            <textarea placeholder="Enter your Specification" />
+            <input placeholder=" Company Location" type="text" />
+            <input placeholder=" Contact Number" type="text" />
+            <input placeholder=" E-mail" type="text" />
+            <textarea placeholder=" Job Specification" />
             <Button1 title='Submit'/>
      </div>  
     )
